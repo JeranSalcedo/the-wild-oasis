f289a144c44fbf921c7a43f5c62aebe19ad112cc
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-18 10:20:33 +0800

    feat(ui): add reusable button component

---
+++ src/ui/Button.jsx
@@ -0,0 +1,65 @@
+import styled, { css } from "styled-components";
+
+const sizes = {
+	small: css`
+		font-size: 1.2rem;
+		font-weight: 600;
+		padding: 0.4rem 0.8rem;
+		text-align: center;
+		text-transform: uppercase;
+	`,
+	medium: css`
+		font-size: 1.4rem;
+		font-weight: 500;
+		padding: 1.2rem 2.4rem;
+	`,
+	large: css`
+		font-size: 1.6rem;
+		font-weight: 500;
+		padding: 1.2rem 2.4rem;
+	`,
+};
+
+const variations = {
+	primary: css`
+		background-color: var(--color-brand-600);
+		color: var(--color-brand-50);
+
+		&:hover {
+			background-color: var(--color-brand-700);
+		}
+	`,
+	secondary: css`
+		background-color: var(--color-gray-0);
+		border: 1px solid var(--color-gray-200);
+		color: var(--color-gray-600);
+
+		&:hover {
+			background-color: var(--color-gray-50);
+		}
+	`,
+	danger: css`
+		background-color: var(--color-red-700);
+		color: var(--color-red-100);
+
+		&:hover {
+			background-color: var(--color-red-800);
+		}
+	`,
+};
+
+const Button = styled.button`
+	border-radius: var(--border-radius-sm);
+	border: none;
+	box-shadow: var(--shdow-sm);
+
+	${(props) => sizes[props.size]}
+	${(props) => variations[props.variation]}
+`;
+
+Button.defaultProps = {
+	size: "medium",
+	variation: "primary",
+};
+
+export default Button;
