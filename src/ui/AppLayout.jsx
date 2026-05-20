427414185ab41aa2972e0e4a3d09b544e017d369
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-20 09:10:32 +0800

    style(layout): add wrapping container

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -16,13 +16,23 @@ const Main = styled.main`
 	padding: 4rem 4.8rem 6.4rem;
 `;
 
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 3.2rem;
+	margin: 0 auto;
+	max-width: 120rem;
+`;
+
 const AppLayout = () => {
 	return (
 		<StyledAppLayout>
 			<Header />
 			<Sidebar />
 			<Main>
-				<Outlet />
+				<Container>
+					<Outlet />
+				</Container>
 			</Main>
 		</StyledAppLayout>
 	);
