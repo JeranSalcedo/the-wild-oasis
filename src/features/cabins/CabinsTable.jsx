a6d4d9d6d06b60ac6a4cfa6a6a4a6820b6e4d041
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-20 15:40:39 +0800

    refactor(hooks): extract fetch cabins logic into custom hook

--- src/features/cabins/CabinsTable.jsx
+++ src/features/cabins/CabinsTable.jsx
@@ -1,7 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 
-import { getCabins } from "../../services/apiCabins";
+import { useCabins } from "./useCabins";
 
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
@@ -29,10 +28,7 @@ const TableHeader = styled.header`
 `;
 
 const CabinsTable = () => {
-	const { data: cabins, isLoading } = useQuery({
-		queryKey: ["cabins"],
-		queryFn: getCabins,
-	});
+	const { isLoading, cabins } = useCabins();
 
 	if (isLoading) return <Spinner />;
 
