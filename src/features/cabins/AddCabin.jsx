cfdc041a785f5e298629b177eef23c0805e2a3c4
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-21 13:44:55 +0800

    style(component): add wrapper div for styling control

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,14 +4,16 @@ import Modal from "../../ui/Modal";
 
 const AddCabin = () => {
 	return (
-		<Modal>
-			<Modal.Open window="cabin-form">
-				<Button>Add new cabin</Button>
-			</Modal.Open>
-			<Modal.Window name="cabin-form">
-				<CabinForm />
-			</Modal.Window>
-		</Modal>
+		<div>
+			<Modal>
+				<Modal.Open window="cabin-form">
+					<Button>Add new cabin</Button>
+				</Modal.Open>
+				<Modal.Window name="cabin-form">
+					<CabinForm />
+				</Modal.Window>
+			</Modal>
+		</div>
 	);
 };
 
