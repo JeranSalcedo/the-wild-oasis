877e80a6671331b569307be6a43a5cf87f36e35b
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-26 09:34:10 +0800

    feat(styling): add alternative prop-based styles

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,29 +1,42 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledFormRow = styled.div`
-	align-items: center;
-	display: grid;
-	gap: 2.4rem;
-	grid-template-columns: 24rem 1fr 1.2fr;
-	padding: 2.4rem 0;
+	${(props) =>
+		props.type === "horizontal" &&
+		css`
+			align-items: center;
+			display: grid;
+			gap: 2.4rem;
+			grid-template-columns: 24rem 1fr 1.2fr;
+			padding: 2.4rem 0;
 
-	&:first-child {
-		padding-top: 0;
-	}
+			&:first-child {
+				padding-top: 0;
+			}
 
-	&:last-child {
-		padding-bottom: 0;
-	}
+			&:last-child {
+				padding-bottom: 0;
+			}
 
-	&:not(:last-child) {
-		border-bottom: 1px solid var(--color-gray-100);
-	}
+			&:not(:last-child) {
+				border-bottom: 1px solid var(--color-gray-100);
+			}
 
-	&:has(button) {
-		display: flex;
-		gap: 1.2rem;
-		justify-content: flex-end;
-	}
+			&:has(button) {
+				display: flex;
+				gap: 1.2rem;
+				justify-content: flex-end;
+			}
+		`}
+
+	${(props) =>
+		props.type === "vertical" &&
+		css`
+			display: flex;
+			flex-direction: column;
+			gap: 0.8rem;
+			padding: 1.2rem 0;
+		`}
 `;
 
 const Label = styled.label`
@@ -35,9 +48,9 @@ const Error = styled.span`
 	font-size: 1.4rem;
 `;
 
-const FormRow = ({ label, error, children }) => {
+const FormRow = ({ type = "horizontal", label, error, children }) => {
 	return (
-		<StyledFormRow>
+		<StyledFormRow type={type}>
 			{label && <Label htmlFor={children.props.id}>{label}</Label>}
 			{children}
 			{error && <Error>{error}</Error>}
