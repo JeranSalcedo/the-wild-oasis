64406214d39164a1eef5128559ce4652b219374a
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-27 09:21:37 +0800

    feat(users): add header user component

---
+++ src/features/users/HeaderUser.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+import { useUserProfile } from "./useUserProfile";
+
+import SpinnerMini from "../../ui/SpinnerMini";
+
+const StyledHeaderUser = styled.div`
+	align-items: center;
+	background: none;
+	border: none;
+	border-radius: var(--border-radius-sm);
+	color: var(--color-gray-600);
+	display: flex;
+	font-weight: 500;
+	font-size: 1.4rem;
+	gap: 1.2rem;
+	padding: 0.6rem;
+	transition: all 0.2s;
+
+	&:hover {
+		background-color: var(--color-gray-100);
+		color: var(--color-brand-700);
+	}
+`;
+
+const Avatar = styled.img`
+	aspect-ratio: 1;
+	border-radius: 50%;
+	display: block;
+	height: 3.6rem;
+	object-fit: cover;
+	object-position: center;
+	outline: 2px solid var(--color-gray-100);
+	width: 4rem;
+`;
+
+const HeaderUser = () => {
+	const { isLoading, profile: { name, avatar_url: avatarURL } = {} } =
+		useUserProfile();
+
+	return (
+		<StyledHeaderUser>
+			{isLoading ? (
+				<SpinnerMini />
+			) : (
+				<>
+					<Avatar
+						src={avatarURL || "default-user.jpg"}
+						alt={`Avatar of ${name}`}
+					/>
+					<span>{name}</span>
+				</>
+			)}
+		</StyledHeaderUser>
+	);
+};
+
+export default HeaderUser;
