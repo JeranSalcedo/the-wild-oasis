18b860747278117bee306041437ca33ba8f359ed
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-05-28 10:47:19 +0800

    feat(dashboard): add box container component

---
+++ src/features/dashboard/DashboardBox.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const DashboardBox = styled.div`
+	background-color: var(--color-gray-0);
+	border: 1px solid var(--color-gray-100);
+	border-radius: var(--border-radius-md);
+	display: flex;
+	flex-direction: column;
+	gap: 2.4rem;
+	padding: 3.2rem;
+`;
+
+export default DashboardBox;
